0a75ca93692d6bce77dd2330acc27d662ca5980a
theskyland <aradioactiveplay@gmail.com>
2025-03-19 11:39:20 +0300

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  const [A, setA] = useState('')
-  const Result = Number(A)
 
   return (
     <View
@@ -11,12 +9,8 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <div> result: {A}</div>
-      <input value={A} onChange={e => setA(e.target.value)} type="number"></input>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <button onClick={() => setA(Result + 10)}></button>
+      }}>
+        <Text>dskpfdlk</Text>
     </View>
   );
 }
