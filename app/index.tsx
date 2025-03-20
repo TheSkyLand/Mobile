33c400ecb23b6573d6a5651bc561cf80f677b078
George <77775699+TheSkyLand@users.noreply.github.com>
2025-03-20 11:53:19 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,31 @@
 import { useState } from "react";
 import { Text, View } from "react-native";
+import { Ref } from "react";
+
 
 export default function Index() {
+  let seconds = 0
+  let minutes = 0
+  let hours = 0
+
+  const timerStart = () => { 
+
+    while (seconds = 59) {
+      seconds++
+      console.log(seconds)
+      console.log(minutes)
+      console.log(hours)
+      if (seconds = 59) {
+        seconds = 0
+        minutes++
+        if (minutes = 59) {
+          minutes = 0
+          hours++
+        }
+      }
+
+    }
+  }
 
   return (
     <View
@@ -10,7 +34,8 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}>
-        <Text>dskpfdlk</Text>
+      <button onClick={timerStart}>START</button>
+      <Text>seconds:{seconds}, minutes:{minutes}, hours:{hours}</Text>
     </View>
   );
 }
