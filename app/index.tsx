5be8fea39ecf7d15397e1d327f189567e1b37c83
theskyland <aradioactiveplay@gmail.com>
2025-04-08 11:38:17 +0300

    porabotal tipo hz

--- app/index.tsx
+++ app/index.tsx
@@ -4,108 +4,43 @@ import { Ref } from "react";
 import { Button } from "react-native";
 import { StyleSheet, TextInput } from 'react-native';
 import React from "react";
+import { useEffect } from "react";
 
 
 
 
-/*
 export default function Index() {
-  const [seconds, setSeconds] = React.useState('0')
-  const [minutes, setMinutes] = React.useState('0')
-  const [hours, setHours] = React.useState('0')
-  function TimerStart() {
-    const timerID = setInterval(function run() {
-      let p = seconds + 1;
-      console.log(p);
-      setSeconds(p);
-      console.log(seconds)
-      //clearInterval(timerID);
-    }, 1000)
-  }
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
 
+  const deadline = seconds
+  const getTime = () => {
+    const time = deadline;
 
-  const [MaxValue, setMaxValue] = React.useState('60')
-  function Limiter(e: any) {
-    console.log(e)
-  }
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
 
+  useEffect(() => {
+    const interval = setInterval(() => getTime(), 1000);
 
+    return () => clearInterval(interval);
+  }, []);
   return (
+    <View>
+      < Input  />
 
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <TextInput
-        placeholder="hours"
-        onChangeText={setHours}
-        value={hours}
-        keyboardType="numeric"
-      />
-      <TextInput
-        placeholder="minutes"
-        onChangeText={setMinutes}
-        value={minutes}
-        keyboardType="numeric"
-      />
-      <TextInput
-        placeholder="seconds"
-        //onChangeText={(e) => Limiter(e)}
-        onChangeText={setSeconds}
-        value={seconds}
-        keyboardType="numeric"
-      />
-      <Button
-        title="start"
-        onPress={TimerStart}
-      />
-      <Text>{seconds}</Text>
-      <Text>{seconds}:{minutes}:{hours}</Text>
+      <Text>{hours < 10 ? "0" + hours : hours}</Text>
+      <Text>Hours</Text>
+      <Text >{minutes < 10 ? "0" + minutes : minutes}</Text>
+      <Text>Minutes</Text>
+      <Text >{seconds < 10 ? "0" + seconds : seconds}</Text>
+      <Text>Seconds</Text>
+
+      <button>START</button>
     </View>
   );
 }
-  */
-
-
-
-// @ts-ignore
-export default function Time({ time }) {
-  
-  const transformMinutes = () => {
-    const convertedValue = Math.floor((time / 60000) % 60);
-    const formattedValue = ("0" + convertedValue).slice(-2)
-    return formattedValue
-  }
-  const transformSeconds = () => {
-    const convertedValue = Math.floor((time / 1000) % 60);
-    const formattedValue = ("0" + convertedValue).slice(-2)
-    return formattedValue
-  }
-  const transformMilliseconds = () => {
-    const convertedValue = Math.floor((time / 10) % 100);
-    const formattedValue = ("0" + convertedValue).slice(-2)
-    return formattedValue
-  }
-  return (
-    <View style={styles.row}>
-      <Text style={styles.time}>
-        {transformMinutes()} :
-        <Text> </Text>{transformSeconds()} :
-        <Text> </Text>{transformMilliseconds()}
-      </Text>
-    </View>
-  )
-}
-const styles = StyleSheet.create({
-  row: {
-    display: 'flex',
-  },
-  time: {
-    fontSize: 48,
-    fontWeight: 'bold',
-    color: '#272727'
-  }
-})
