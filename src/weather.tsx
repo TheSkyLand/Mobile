86181320e88720cb58712ab5fe6c11f8052f745e
TheSkyLandOSK <Aradioactiveplay@gmail.com>
2025-11-01 14:47:05 +0300

    update

--- src/weather.tsx
+++ src/weather.tsx
@@ -2,46 +2,68 @@ import React, { useState } from "react"
 import { View, Text } from "react-native"
 import { useEffect } from "react"
 import Geolocation from '@react-native-community/geolocation'
+import { WeatherDto } from "./type/weather.type"
+import { ActivityIndicator } from "react-native"
 
+const Weather = () => {
+    const [current, setCurrent] = useState<[]>([])
+    const [forecast, setForecast] = useState<[]>([])
 
-type WeatherProps = {
-    id: Number,
-    current: String,
-    description: String
-}
-
-const Weather = (props: WeatherProps) => {
-    const [data, setData] = useState<WeatherProps[]>([])
+    const [data, setData] = useState<[]>();
 
     const [latitude, setLatitude] = useState(22.22);
     const [longitude, setLongitude] = useState(22.22);
 
-    const GetWeather = () => {
-        return fetch(`{https//api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=05f222c0b775a252d1b0fd722b28b0c8}`)
+    const GetWeatherToday = () => {
+        return fetch("https://api.openweathermap.org/data/2.5/weather?lat=22&lon=22&appid=05f222c0b775a252d1b0fd722b28b0c8")
             .then(response => response.json())
             .then(json => {
-                setData(json.weather)
+                console.log(json.weather)
+                setCurrent(json.current)
                 return json.weather;
             })
             .catch(error => {
                 console.log(error)
             })
     }
+
+    const GetWeatherForecast = () => {
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=22&lon=22&appid=05f222c0b775a252d1b0fd722b28b0c8`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json.forecast)
+                setForecast(json.forecast)
+                return json.forecast
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     useEffect(() => {
-        GetWeather()
+        GetWeatherToday()
+        GetWeatherForecast()
     }, [])
+
     Geolocation.getCurrentPosition(info => console.log(info));
 
     return (
         <View>
-            <Text>
-                {props.current}
+            {data ?
+                data.map((item: WeatherDto, index) => (
 
-            </Text>
-            <Text>
-                {props.description}
-            </Text>
+                    <View key={index} style={{ padding: 20 }}>
+                        <Text> погода {item.conditions} </Text>
+                        <Text> погода {item.datetime} </Text>
+                        <Text> погода {item.sunset} </Text>
+                        <Text> погода {item.temp} </Text>
+                    </View>
+                ))
+                :
+                <ActivityIndicator size="large" color="#0000ff" />
+            }
         </View>
+
     )
 }
 
