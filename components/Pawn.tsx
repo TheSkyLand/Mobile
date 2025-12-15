fbf554d32d408c5449e9b817ae981382032e8d44
George <77775699+TheSkyLand@users.noreply.github.com>
2025-12-15 15:08:08 +0300

    Update Pawn.tsx

--- components/Pawn.tsx
+++ components/Pawn.tsx
@@ -1,30 +1,28 @@
 import { TouchableOpacity, View } from "react-native"
 
 interface PawnProps {
-    keyNum: number;
-
+    keyNum: number[];
 }
 
 const Pawn = (props: PawnProps) => {
-
+    const test = (inpt: any) => {
+        console.log(inpt);
+    }
     return (
         <TouchableOpacity
+            onPress={() => test(props.keyNum)}
             style={{
-                backgroundColor: "black",
-                borderRadius: 50,
-                borderWidth: 5,
-                shadowColor: "grey",
-                borderColor: "grey"
+                width: "100%",
+                height: "100%"
             }}
         >
             <View
             style={{
-                backgroundColor: "white"
+                backgroundColor: "gray"
             }}
             >
                 {props.keyNum}
             </View>
-
         </TouchableOpacity>
     )
 }
